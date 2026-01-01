ab2cc88dfa2e4b91a89ba2e79e242be347eb7a75
Sultana247 <jahansultana302@gmail.com>
2026-01-01 16:15:04 +0600

    working on header

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+import { Outlet } from 'react-router';
+
+const Home = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,21 @@ import App from './App.jsx'
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Home from './components/Home.jsx';
+import Root from './components/Root.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Root></Root>,
+    children:[
+      {
+       path:'/',
+       element:<Home></Home>
+      }
+    ]
+    
+   
   },
 ]);
 
