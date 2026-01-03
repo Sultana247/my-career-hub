37e69e43c5b520ea679f555747675147682d5a07
Sultana247 <jahansultana302@gmail.com>
2026-01-03 11:38:57 +0600

    added featured jobs and category

--- src/components/FeateuredJobs.jsx
+++ src/components/FeateuredJobs.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Jobs from './Jobs';
 
 const FeateuredJobs = () => {
+    const [jobs, setJobs] = useState([])
+    useEffect(()=>{
+        fetch('jobs.json')
+        .then(res=>res.json())
+        .then(data=>setJobs(data))
+    },
+        [])
     return (
         <div>
              <div className='mt-32 max-w-7xl mx-auto '>
@@ -8,7 +16,18 @@ const FeateuredJobs = () => {
                     <h3 className='font-extrabold text-5xl '>Featured Jobs</h3>
                     <p className='font-medium text-[16px] text-[#757575]'>Explore thousands of job opportunities with all the information you need. Its your future</p>
                 </div>
-               
+                          {/*Jobs display*/}
+                         
+                         <div className='grid grid-cols-2 gap-6'>
+                             {
+                            jobs.map(job=><Jobs job={job} key={job.id}></Jobs>)
+                          }
+
+                          
+                         </div>
+                         <div className='flex justify-center mt-10 mb-32'>
+                            <button className="rounded-lg bg-linear-to-r from-[#7E90FE] to-[#9873FF] font-bold text-xl text-white px-4 py-2.5 ">See all jobs</button>
+                          </div>
             </div>
         </div>
     );
---
+++ src/components/Jobs.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CiLocationOn, CiDollar} from "react-icons/ci";
+
+const Jobs = (jobs) => {
+    const job = jobs.job;
+    const { job_title, company_name, job_type, logo, remote_or_onsite, location, salary} = job;
+    return (
+        <div>
+            <div className=" bg-base-100 shadow-sm p-10">
+                <figure>
+                    <img className='w-29 h-10'
+                    src={logo}
+                    alt="Shoes" />
+                </figure>
+                <div className=" mt-8">
+                    <h2 className="font-extrabold text-2xl text-[#474747]">{job_title}</h2>
+                    <p className='font-semibold text-xl text-[#757575] mt-2 mb-4'>{company_name}</p>
+                    <div className='flex gap-6 mb-4'>
+                        <button className='border text-[#7E90FE] py-2.5 px-4.5 rounded-sm'>{remote_or_onsite}</button>
+                        <button className='border text-[#7E90FE] py-2.5 px-4.5 rounded-sm'>{job_type}</button>
+                    </div>
+                    <div className='flex gap-6 text-[#757575] text-xl font-semibold mb-4 '>
+                        <p className='flex gap-2 justify-center items-center'><CiLocationOn />{location}</p>
+                        <p className='flex gap-2 justify-center items-center'><CiDollar />Salary: {salary}</p>
+                    </div>
+                    <div className="">
+                    <button className="rounded-sm bg-linear-to-r from-[#7E90FE] to-[#9873FF] font-bold text-xl text-white px-4 py-2.5 ">View details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Jobs;
